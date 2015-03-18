a8905779327714678db7990b23a8f8fee3fe9c81
Nat Zimmermann <nat@natzim.me>
2015-03-18 12:37:44 +0000

    Updated models

--- app/Playlist.php
+++ app/Playlist.php
@@ -8,18 +8,19 @@ use Illuminate\Database\Eloquent\Model;
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Song[] $songs
  * @property-read \App\Playlist $forkParent
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Playlist[] $forks
- * @property integer $id 
- * @property integer $user_id 
- * @property integer $forked_playlist_id 
- * @property string $name 
- * @property \Carbon\Carbon $created_at 
- * @property \Carbon\Carbon $updated_at 
+ * @property integer $id
+ * @property integer $user_id
+ * @property integer $forked_playlist_id
+ * @property string $name
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
  * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereId($value)
  * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereUserId($value)
  * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereForkedPlaylistId($value)
  * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereName($value)
  * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereCreatedAt($value)
  * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereUpdatedAt($value)
+ * @property-read \App\User $author 
  */
 class Playlist extends Model {
 
@@ -38,6 +39,11 @@ class Playlist extends Model {
     public function forks()
     {
         return $this->hasMany('App\Playlist', 'fork_parent_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
     }
 
 }
--- app/User.php
+++ app/User.php
@@ -9,13 +9,13 @@ use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 /**
  * App\User
  *
- * @property integer $id 
- * @property string $name 
- * @property string $email 
- * @property string $password 
- * @property string $remember_token 
- * @property \Carbon\Carbon $created_at 
- * @property \Carbon\Carbon $updated_at 
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string $remember_token
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
  * @method static \Illuminate\Database\Query\Builder|\App\User whereId($value)
  * @method static \Illuminate\Database\Query\Builder|\App\User whereName($value)
  * @method static \Illuminate\Database\Query\Builder|\App\User whereEmail($value)
@@ -23,30 +23,19 @@ use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
  * @method static \Illuminate\Database\Query\Builder|\App\User whereRememberToken($value)
  * @method static \Illuminate\Database\Query\Builder|\App\User whereCreatedAt($value)
  * @method static \Illuminate\Database\Query\Builder|\App\User whereUpdatedAt($value)
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Playlist[] $playlists 
  */
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
 	use Authenticatable, CanResetPassword;
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
 	protected $table = 'users';
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
-	protected $fillable = ['name', 'email', 'password'];
-
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
 	protected $hidden = ['password', 'remember_token'];
+
+    public function playlists()
+    {
+        return $this->hasMany('App\Playlist');
+    }
 
 }
