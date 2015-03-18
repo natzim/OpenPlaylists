48b9e8c7d1e14f8a4ab74f09a18cfe3fcc2bb43e
Nat Zimmermann <nat@natzim.me>
2015-03-18 12:29:54 +0000

    Added playlist show

--- resources/views/playlists/show.blade.php
+++ resources/views/playlists/show.blade.php
@@ -1,5 +1,26 @@
 @extends('app')
 
 @section('content')
+<div class="container">
+    <div class="row">
+        <h1>{{ $playlist->name }}</h1>
+        @if(!empty($playlist->forkParent))
+            <p class="text-muted">
+                <i class="fa fa-code-fork"></i>
+                Fork of <a href="{{ route('playlists.show', [$playlist]) }}">
+                    {{ $playlist->forkParent->name }}
+                </a>
+            </p>
+        @endif
+        <a href="" class="btn btn-success" data-toggle="tooltip" data-placement="bottom" title="Fork this playlist">
+            <i class="fa fa-code-fork"></i>
+            <span class="sr-only">Fork this playlist</span>
+        </a>
+        @forelse($playlist->songs as $song)
 
+        @empty
+            <p class="lead">Sorry, we couldn't find any songs!</p>
+        @endforelse
+    </div>
+</div>
 @stop
