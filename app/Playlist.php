2b2529fdf3cf14637751fa824a5a6df44ce0e2ce
Nat Zimmermann <nat@natzim.me>
2015-03-18 10:47:23 +0000

    Added IDE helper

--- app/Playlist.php
+++ app/Playlist.php
@@ -2,6 +2,25 @@
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Playlist
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Song[] $songs
+ * @property-read \App\Playlist $forkParent
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Playlist[] $forks
+ * @property integer $id 
+ * @property integer $user_id 
+ * @property integer $forked_playlist_id 
+ * @property string $name 
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereForkedPlaylistId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereUpdatedAt($value)
+ */
 class Playlist extends Model {
 
 	protected $table = 'playlists';
--- app/Song.php
+++ app/Song.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Song
+ *
+ * @property-read \App\Playlist $playlist
+ * @property integer $id 
+ * @property integer $playlist_id 
+ * @property string $url 
+ * @method static \Illuminate\Database\Query\Builder|\App\Song whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Song wherePlaylistId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Song whereUrl($value)
+ */
 class Song extends Model {
 
 	protected $table = 'songs';
