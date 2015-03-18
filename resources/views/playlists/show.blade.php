5b270c36485851843e6f01c8854c81d191ca090f
Nat Zimmermann <nat@natzim.me>
2015-03-18 18:31:27 +0000

    Started to work on play buttons for specific songs

--- resources/views/playlists/show.blade.php
+++ resources/views/playlists/show.blade.php
@@ -3,26 +3,48 @@
 @section('content')
 <div class="container">
     <div class="row" data-playlist-id="{{ $playlist->id }}">
-        <h1>{{ $playlist->name }}</h1>
-        @if(!empty($playlist->forkParent))
-            <p class="text-muted">
-                <i class="fa fa-code-fork"></i>
-                Fork of <a href="{{ route('playlists.show', [$playlist]) }}">
-                    {{ $playlist->forkParent->name }}
+        <div class="row">
+            <div class="col-md-6">
+                <h1>{{ $playlist->name }}</h1>
+                @if(!empty($playlist->forkParent))
+                    <p class="text-muted">
+                        <i class="fa fa-code-fork"></i>
+                        Fork of <a href="{{ route('playlists.show', [$playlist]) }}">
+                            {{ $playlist->forkParent->name }}
+                        </a>
+                    </p>
+                @endif
+                <p>
+                    <i class="fa fa-user"></i>
+                    Author: {{ $playlist->user->name }}
+                </p>
+                <a href="" class="btn btn-success" data-toggle="tooltip" data-placement="bottom" title="Fork this playlist">
+                    <i class="fa fa-code-fork fa-lg"></i>
+                    <span class="sr-only">Fork this playlist</span>
                 </a>
-            </p>
-        @endif
-        <p>
-            <i class="fa fa-user"></i>
-            Author: {{ $playlist->user->name }}
-        </p>
-        <a href="" class="btn btn-success" data-toggle="tooltip" data-placement="bottom" title="Fork this playlist">
-            <i class="fa fa-code-fork"></i>
-            <span class="sr-only">Fork this playlist</span>
-        </a>
-        <hr>
-        <div class="embed-responsive embed-responsive-16by9">
-            <iframe class="embed-responsive-item" id="embed"></iframe>
+            </div>
+            <div class="col-md-6">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" id="embed"></iframe>
+                </div>
+                <hr>
+                <div class="text-center">
+                    <div class="btn-group btn-group-lg" role="group" aria-label="Playlist controls">
+                        <button type="button" class="btn btn-default" id="previous">
+                            <i class="fa fa-fast-backward"></i>
+                            <span class="sr-only">Previous song</span>
+                        </button>
+                        <button type="button" class="btn btn-default" id="play">
+                            <i class="fa fa-play"></i>
+                            <span class="sr-only">Play/Pause song</span>
+                        </button>
+                        <button type="button" class="btn btn-default" id="next">
+                            <i class="fa fa-fast-forward"></i>
+                            <span class="sr-only">Next song</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
         <hr>
         <div class="list-container"></div>
@@ -32,6 +54,9 @@
 
 @section('scripts')
 <script>
+    var songs = [];
+    var currentSong = 0;
+
     function extractEmbeddableUrlFromHtml(html, site) {
         var segments = html.split(' ');
         var url = '';
@@ -61,7 +86,6 @@
         $.ajax({
             url: playlistId + '/songs',
             success: function(urls) {
-                var songs = [];
                 var requests = [];
 
                 for (var i = 0; i < urls.length; i++) {
@@ -82,25 +106,38 @@
                 $.when.apply(null, requests).done(function() {
                     for (var i = 0; i < songs.length; i++) {
                         var song = songs[i];
-                        var faSite = '';
-
-                        switch (song.site) {
-                            case 'YouTube':
-                                faSite = 'youtube-play';
-                                break;
-                            case 'SoundCloud':
-                                faSite = 'cloud';
-                                break;
-                        }
 
                         $('.list-container').append(
-                            '<h2><i class="fa fa-' + faSite + '"></i> ' + song.title + '</h2>'
+                            '<div class="well well-sm">' +
+                                '<div class="row">' +
+                                    '<div class="col-xs-1">' +
+                                        '<button type="button" class="btn btn-block btn-default" data-song-index="' + i + '">' +
+                                            '<i class="fa fa-play"></i>' +
+                                        '</button>' +
+                                    '</div>' +
+                                    '<div class="col-xs-11">' +
+                                        '<p>' + song.title + '</p>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>'
                         );
                     }
-                    $('#embed').attr('src', songs[0].url);
                 });
             }
         });
+    });
+
+    $(document).on('click', '[data-song-index]', function() {
+        var songIndex = $(this).data('song-index');
+
+        if (songIndex !== currentSong) {
+            $('#embed').attr('src', songs[songIndex].url);
+            currentSong = songIndex;
+            $(this).html('<i class="fa fa-pause"></i>');
+        } else {
+            // TODO: Pause music
+            $(this).html('<i class="fa fa-play"></i>');
+        }
     });
 </script>
 @stop
