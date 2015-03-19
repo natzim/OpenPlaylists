61553a1a1c24e69af3c2ad2c3943552fdd544180
Nat Zimmermann <nat@natzim.me>
2015-03-19 17:11:53 +0000

    Added some error pages

---
+++ resources/views/errors/418.blade.php
@@ -0,0 +1,6 @@
+@extends('errors.base')
+
+@section('error')
+<h1>I'm a teapot</h1>
+<p class="lead">418</p>
+@stop
---
+++ resources/views/errors/base.blade.php
@@ -0,0 +1,9 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+    <div class="text-center">
+        @yield('error')
+    </div>
+</div>
+@stop
