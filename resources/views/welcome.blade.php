7bd87ae0238503f189d2145b1784b513106902f9
Nat Zimmermann <nat@natzim.me>
2015-03-19 15:10:41 +0000

    Updated landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
 <div class="container">
-    <div class="page-header">
-        <h1>OpenPlaylists</h1>
+    <div class="text-center">
+        <p class="lead">OpenPlaylists is a simple way of sharing music playlists.</p>
+        <a class="btn btn-primary btn-lg" href="{{ route('playlists.index') }}">View playlists</a>
     </div>
 </div>
 @stop
