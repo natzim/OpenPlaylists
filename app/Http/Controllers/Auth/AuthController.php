766cae202069b040f513c7e7a6d6dddc77440662
Nat Zimmermann <nat@natzim.me>
2015-03-19 15:26:46 +0000

    Changed auth rules

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\Registrar;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller {
 
@@ -36,5 +37,33 @@ class AuthController extends Controller {
 
 		$this->middleware('guest', ['except' => 'getLogout']);
 	}
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param Request $request
+     *
+     * @return $this|\Illuminate\Http\RedirectResponse
+     */
+    public function postLogin(Request $request)
+    {
+        $this->validate($request, [
+            'name'     => 'required',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('name', 'password');
+
+        if ($this->auth->attempt($credentials, $request->has('remember')))
+        {
+            return redirect()->intended($this->redirectPath());
+        }
+
+        return redirect($this->loginPath())
+            ->withInput($request->only('name', 'remember'))
+            ->withErrors([
+                'name' => $this->getFailedLoginMessage(),
+            ]);
+    }
 
 }
