3aab6c3e324339fd5720cf39ca607797be75cc20
Nat Zimmermann <nat@natzim.me>
2015-04-05 18:45:07 +0100

    Removed IDE helper dependency

--- app/Playlist.php
+++ app/Playlist.php
@@ -5,34 +5,6 @@ use Cviebrock\EloquentSluggable\SluggableInterface;
 use Cviebrock\EloquentSluggable\SluggableTrait;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-/**
- * App\Playlist
- *
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Song[]     $songs
- * @property-read \App\Playlist                                            $forkParent
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Playlist[] $forks
- * @property integer                                                       $id
- * @property integer                                                       $user_id
- * @property integer                                                       $forked_playlist_id
- * @property string                                                        $name
- * @property \Carbon\Carbon                                                $created_at
- * @property \Carbon\Carbon                                                $updated_at
- * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereId($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereUserId($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereForkedPlaylistId($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereName($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereCreatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereUpdatedAt($value)
- * @property-read \App\User                                                $author
- * @property integer                                                       $fork_parent_id
- * @property-read \App\User                                                $user
- * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereForkParentId($value)
- * @property string                                                        $slug
- * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereSlug($value)
- * @property string                                                        $deleted_at
- * @method static \Illuminate\Database\Query\Builder|\App\Playlist whereDeletedAt($value)
- * @method static \App\Playlist findBySlugOrFail($slug)
- */
 class Playlist extends Model implements SluggableInterface {
 
     use SluggableTrait;
--- app/Song.php
+++ app/Song.php
@@ -2,21 +2,6 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * App\Song
- *
- * @property-read \App\Playlist $playlist
- * @property integer            $id
- * @property integer            $playlist_id
- * @property string             $url
- * @method static \Illuminate\Database\Query\Builder|\App\Song whereId($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Song wherePlaylistId($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Song whereUrl($value)
- * @property string             $youtube_id
- * @method static \Illuminate\Database\Query\Builder|\App\Song whereYoutubeId($value)
- * @property string             $name
- * @method static \Illuminate\Database\Query\Builder|\App\Song whereName($value)
- */
 class Song extends Model {
 
     protected $table = 'songs';
