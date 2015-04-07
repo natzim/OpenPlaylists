2aaa61536e4ec3f61f676c8d582714f534d3c164
Nat Zimmermann <nat@natzim.me>
2015-04-07 19:58:31 +0100

    Added genre to playlist list

--- resources/views/partials/playlistlist.blade.php
+++ resources/views/partials/playlistlist.blade.php
@@ -2,6 +2,9 @@
     @foreach($playlists as $playlist)
         <div class="list-group-item">
             <a href="{{ route('playlists.show', $playlist->slug) }}">{{ $playlist->name }}</a>
+            @if (!empty($playlist->genre->name))
+                <span class="label label-default">{{ $playlist->genre->name }}</span>
+            @endif
             <span class="badge">{{ $playlist->songs->count() }}</span>
         </div>
     @endforeach
