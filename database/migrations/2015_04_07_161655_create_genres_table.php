1a5e3dbf2336d1e3f389c84b3a8d02b0728d1f64
Nat Zimmermann <nat@natzim.me>
2015-04-07 17:27:52 +0100

    Renamed parent genre column

--- database/migrations/2015_04_07_161655_create_genres_table.php
+++ database/migrations/2015_04_07_161655_create_genres_table.php
@@ -16,7 +16,7 @@ class CreateGenresTable extends Migration {
         {
             $table->increments('id');
             $table->string('name', 20);
-            $table->integer('parent_genre')
+            $table->integer('parent_id')
                 ->unsigned();
         });
 	}
