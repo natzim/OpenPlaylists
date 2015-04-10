7dab39633f8837a907f65659df8c19eaa49c30a4
Nat Zimmermann <nat@natzim.me>
2015-04-10 15:51:00 +0100

    Removed support for IE8

--- resources/views/fluid.blade.php
+++ resources/views/fluid.blade.php
@@ -34,13 +34,6 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="msapplication-TileImage" content="/mstile-144x144.png">
     <meta name="theme-color" content="#ffffff">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body>
 <nav class="navbar navbar-default">
