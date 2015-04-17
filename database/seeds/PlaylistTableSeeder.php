6cc891518d82fb55cd5902064783cbc0d2857b62
Nat Zimmermann <nat@natzim.me>
2015-04-17 16:21:02 +0100

    Fixed seeding mass assignment bug

--- database/seeds/PlaylistTableSeeder.php
+++ database/seeds/PlaylistTableSeeder.php
@@ -2,11 +2,15 @@
 
 use Illuminate\Database\Seeder;
 use App\Playlist;
+use Illuminate\Database\Eloquent\Model;
 
 class PlaylistTableSeeder extends Seeder {
 
     public function run()
     {
+        // Don't know why I had to call this again
+        Model::unguard();
+
         DB::table('playlists')->truncate();
 
         $faker = Faker\Factory::create();
